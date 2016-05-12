1c80669126ce4e967da33219172f0cf400a9d429
Calvin Fernandes <calvin.l.fer@gmail.com>
2016-05-11 22:17:58 -0400

    Clean up action handlers (1st pass)

--- app/controllers/PersonController.scala
+++ app/controllers/PersonController.scala
@@ -9,8 +9,8 @@ import models.dto.ErrorResponse._
 import models.dto.{CreatePerson, ErrorResponse}
 import play.api.data.validation.ValidationError
 import play.api.libs.json.{JsPath, JsResult, Json}
-import play.api.mvc.{Action, Controller}
-import services.data.PersonsDAO
+import play.api.mvc.{Action, Controller, Result}
+import services.data.{CreateResult, PersonsDAO}
 
 import scala.concurrent.Future
 
@@ -18,57 +18,6 @@ import scala.concurrent.Future
 class PersonController @Inject()(persons: PersonsDAO) extends Controller {
 
   private implicit val ec = play.api.libs.concurrent.Execution.Implicits.defaultContext
-
-  def create = Action.async(parse.json) {
-    implicit request =>
-      val eitherCreatePerson = validateParsedResult(request.body.validate[CreatePerson])
-      eitherCreatePerson.fold(
-        errorResponse => Future.successful(BadRequest(Json.toJson(errorResponse))), {
-          createPersonReq =>
-            val uuid = UUID.randomUUID()
-            val person = Person(uuid, createPersonReq.firstName, createPersonReq.lastName, createPersonReq.studentId, createPersonReq.gender)
-            val result = persons.create(person)
-
-            result.map(either =>
-              either.fold(
-                error => BadRequest(error.message),
-                createResult =>
-                  Created(
-                    Json.toJson(
-                      Map[String, String](
-                        "status" -> createResult.message,
-                        "id" -> createResult.id.toString
-                      )
-                    )
-                  )
-              )
-            )
-        }
-      )
-  }
-
-  def read(personId: UUID) = Action.async {
-    persons.read(personId)
-      .map(optPerson =>
-        optPerson
-          .map(person => Ok(Json.toJson(person)))
-          .getOrElse(NotFound(s"Person with (id: $personId) does not exist"))
-      )
-  }
-
-  def update(personId: UUID) = TODO
-
-  def delete(personId: UUID) = Action.async {
-    persons.delete(personId)
-      .map(
-        either =>
-          either.fold(
-            doesNotExist => NotFound(s"Person with (id: $personId) does not exist"),
-            deleteResult => Ok(deleteResult.message))
-      )
-  }
-
-  def readAll = Action.async(persons.all.map(results => Ok(Json.toJson(results))))
 
   private def validateParsedResult[T](jsResult: JsResult[T]): Either[ErrorResponse, T] =
     jsResult.fold(
@@ -78,4 +27,49 @@ class PersonController @Inject()(persons: PersonsDAO) extends Controller {
       },
       (t: T) => Right(t)
     )
+
+  private def createPerson(request: CreatePerson): Person =
+    Person(UUID.randomUUID(), request.firstName, request.lastName, request.studentId, request.gender)
+
+  private def asyncHttpBadRequestGivenErrorResponse(errorResponse: ErrorResponse): Future[Result] =
+    Future.successful(BadRequest(Json toJson errorResponse))
+
+  private def httpBadRequestGivenException(exception: Exception) =
+    BadRequest(Json toJson ErrorResponse(code = exception.getMessage, errors = Map.empty))
+
+  private def httpCreatedGivenCreateResult(createResult: CreateResult) =
+    Created(Json.toJson(Map[String, String]("status" -> createResult.message, "id" -> createResult.id.toString)))
+
+  private def persistPersonSendHttpResponse(request: CreatePerson): Future[Result] = {
+    val person = createPerson(request)
+    val dataResult = persons.create(person)
+    dataResult.map(_.fold(httpBadRequestGivenException, httpCreatedGivenCreateResult))
+  }
+
+  private def personDoesNotExistHttpResponse(personId: UUID): Result =
+    NotFound(Json toJson ErrorResponse("Does not Exist", Map("CouldNotFind" -> s"Person with (id: $personId) does not exist")))
+
+  def create = Action.async(parse.json) {
+    implicit request =>
+      val createPersonRequest = validateParsedResult(request.body.validate[CreatePerson])
+      createPersonRequest.fold(asyncHttpBadRequestGivenErrorResponse, persistPersonSendHttpResponse)
+  }
+
+  def read(personId: UUID) = Action.async {
+    persons.read(personId)
+      .map(optPerson =>
+        optPerson
+          .map(person => Ok(Json.toJson(person)))
+          .getOrElse(personDoesNotExistHttpResponse(personId))
+      )
+  }
+
+  def update(personId: UUID) = TODO
+
+  def delete(personId: UUID) = Action.async {
+    persons.delete(personId)
+      .map(_.fold(_ => personDoesNotExistHttpResponse(personId), delResult => Ok(delResult.message)))
+  }
+
+  def readAll = Action.async(persons.all.map(results => Ok(Json.toJson(results))))
 }
