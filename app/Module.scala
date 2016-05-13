f84fa0c82a0a8ce894c3d4b8ae3fef084dd17044
Calvin Fernandes <calvin.l.fer@gmail.com>
2016-05-12 23:24:10 -0400

    Checkpoint: Added comments and a bit of reorg

--- app/Module.scala
+++ app/Module.scala
@@ -1,6 +1,3 @@
-import java.io.{File, PrintWriter}
-
-import com.google.inject.{AbstractModule, Inject, Provider}
 import java.time.Clock
 import javax.inject.{Named, Singleton}
 
@@ -8,38 +5,42 @@ import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.google.inject.name.Names
+import com.google.inject.{AbstractModule, Inject, Provider}
 import play.api.{Configuration, Environment, Logger}
 import services.data._
 
 /**
- * This class is a Guice module that tells Guice how to bind several
- * different types. This Guice module is created when the Play
- * application starts.
-
- * Play will automatically use any class called `Module` that is in
- * the root package. You can create modules in other locations by
- * adding `play.modules.enabled` settings to the `application.conf`
- * configuration file.
- */
+  * This class is a Guice module that tells Guice how to bind several
+  * different types. This Guice module is created when the Play
+  * application starts.
+  *
+  * Play will automatically use any class called `Module` that is in
+  * the root package. You can create modules in other locations by
+  * adding `play.modules.enabled` settings to the `application.conf`
+  * configuration file.
+  */
 class Module(environment: Environment, configuration: Configuration) extends AbstractModule {
 
   override def configure() = {
     // Use the system clock as the default implementation of Clock
     bind(classOf[Clock]).toInstance(Clock.systemDefaultZone)
 
-    // Use the in-memory implementation for PersonsDAO
-    bind(classOf[PersonsRepository]).to(classOf[InMemoryPersonsRepository]).asEagerSingleton()
+    // Use our configured Dynamo client if someone injects @Named("DynamoClient)
     bind(classOf[AmazonDynamoDBClient])
       .annotatedWith(Names.named("DynamoClient"))
       .toProvider(classOf[DynamoDBClientProvider])
       .asEagerSingleton()
+
+    // Use the in-memory implementation for PersonsDAO
+    bind(classOf[PersonsRepository]).to(classOf[InMemoryPersonsRepository]).asEagerSingleton()
   }
 }
 
 @Singleton
 @Named("DynamoClient")
-class DynamoDBClientProvider @Inject() (configuration: Configuration) extends Provider[AmazonDynamoDBClient] {
+class DynamoDBClientProvider @Inject()(configuration: Configuration) extends Provider[AmazonDynamoDBClient] {
   val log = Logger("DynamoDB configuration")
+
   override def get(): AmazonDynamoDBClient = {
     val region = configuration.getString("dynamodb.region").map(r => Regions.fromName(r)).getOrElse(Regions.US_WEST_1)
     val endpoint = configuration.getString("dynamodb.endpoint")
