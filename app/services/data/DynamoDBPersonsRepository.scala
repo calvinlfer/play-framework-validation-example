afbed12ad35c68f9283aa26276fe55f2987004f4
Calvin Fernandes <calvin.l.fer@gmail.com>
2016-05-13 19:04:57 -0400

    Removed injection for DynamoDB from InMemoryPersonsRepository Created a class for DynamoDBPersonsRepository

--- app/services/data/DynamoDBPersonsRepository.scala
+++ app/services/data/DynamoDBPersonsRepository.scala
@@ -1,17 +1,36 @@
 package services.data
 
 import java.util.UUID
+import javax.inject.Inject
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import models.domain.{Person, PersonAlreadyExists, PersonDoesNotExist}
 
 import scala.collection.parallel.mutable
 import scala.concurrent.Future
 
-
-class InMemoryPersonsRepository extends PersonsRepository {
+/**
+  * Note that @Inject is required due to the fact that in order for Guice to work,
+  * classes must have either one (and only one) constructor annotated with @Inject
+  * or a zero-argument constructor that is not private
+  * @param client a fully configured Amazon DynamoDB client
+  */
+class DynamoDBPersonsRepository @Inject()(client: AmazonDynamoDBClient) extends PersonsRepository {
   private val store = mutable.ParTrieMap.empty[UUID, Person]
   private implicit val ec = scala.concurrent.ExecutionContext.Implicits.global
 
+  import com.gu.scanamo._
+//  val createTableRequest = new CreateTableRequest().withTableName("customers")
+//  createTableRequest.withKeySchema(new KeySchemaElement().withAttributeName("customerId").withKeyType(KeyType.HASH))
+//  createTableRequest.withAttributeDefinitions(new AttributeDefinition().withAttributeName("customerId").withAttributeType(ScalarAttributeType.S))
+//  createTableRequest.setProvisionedThroughput(new ProvisionedThroughput(1000L, 1000L))
+//  client.createTable(createTableRequest)
+  val tables = client.listTables(10)
+  println(tables)
+
+  case class Farmer(customerId: String, animals: List[String])
+  val putResult = Scanamo.put(client)("customers")(Farmer("Ol Mcdonald", List("sheep", "cow")))
+  println(putResult)
 
   override def create(person: Person): Future[Either[PersonAlreadyExists, CreateResult]] =
     Future {
