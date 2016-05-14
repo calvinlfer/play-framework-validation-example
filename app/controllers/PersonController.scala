fb004b0ed1d6f93dbf2fadb680c88b52b798547f
Calvin Fernandes <calvin.l.fer@gmail.com>
2016-05-14 00:33:50 -0400

    formattin'

--- app/controllers/PersonController.scala
+++ app/controllers/PersonController.scala
@@ -5,9 +5,9 @@ import javax.inject.Inject
 
 import com.google.inject.Singleton
 import models.domain.{Person => PersonModel}
-import models.dto.{CreatePerson, ErrorResponse, UpdatePerson}
 import models.dto.ErrorResponse._
 import models.dto.Person._
+import models.dto.{CreatePerson, ErrorResponse, UpdatePerson}
 import play.api.Logger
 import play.api.libs.json._
 import play.api.mvc.{Action, Controller, Result}
@@ -23,32 +23,32 @@ class PersonController @Inject()(persons: PersonsRepository) extends Controller 
 
   private def validateJsonBody[T](jsonBody: JsValue)(implicit reads: Reads[T]): Either[ErrorResponse, T] =
     jsonBody.validate[T].asEither.fold(
-      errors  =>    Left(ErrorResponse("Validation Error", fmtValidationResults(errors))),
-      t       =>    Right(t)
+      errors => Left(ErrorResponse("Validation Error", fmtValidationResults(errors))),
+      t => Right(t)
     )
 
   private def onValidationHandleSuccess[T](jsonBody: JsValue)(successFn: T => Future[Result])
-                                    (implicit reads: Reads[T]): Future[Result] =
+                                          (implicit reads: Reads[T]): Future[Result] =
     validateJsonBody(jsonBody).fold(errorResponse => Future.successful(BadRequest(errorResponse.toJson)), successFn)
 
   private val ServiceError: Result = ServiceUnavailable(ErrorResponse("Service Error", Map.empty).toJson)
 
   private def onCreateHandleSuccess[T](futureCreateResult: Future[Either[RepositoryError, CreateResult]])
-                                (completeFn: CreateResult => Result): Future[Result] =
+                                      (completeFn: CreateResult => Result): Future[Result] =
     futureCreateResult.map {
       case Right(createResult) => completeFn(createResult)
       case Left(_) => ServiceError
     }
 
   private def onUpdateHandleSuccess[T](futureUpdateResult: Future[Either[RepositoryError, UpdateResult]])
-                                (completeFn: UpdateResult => Result): Future[Result] =
+                                      (completeFn: UpdateResult => Result): Future[Result] =
     futureUpdateResult.map {
       case Right(updateResult) => completeFn(updateResult)
       case Left(_) => ServiceError
     }
 
   private def onFindHandleSuccess[T](futureFindResult: Future[Either[RepositoryError, Option[PersonModel]]])
-                              (completeFn: PersonModel => Future[Result]): Future[Result] =
+                                    (completeFn: PersonModel => Future[Result]): Future[Result] =
     futureFindResult.flatMap {
       case Right(Some(person)) => completeFn(person)
       case Right(None) => Future.successful(NotFound(ErrorResponse("Resource not found", Map.empty).toJson))
