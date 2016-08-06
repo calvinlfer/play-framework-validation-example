974e101e54415debdbf5e7ed5c1c14907dbe6ba4
Calvin Fernandes <calvin.l.fer@gmail.com>
2016-08-06 19:04:34 -0400

    Use BooPickle to serialize/deserialize

--- app/services/data/dynamodb/DynamoDBFormatHelpers.scala
+++ app/services/data/dynamodb/DynamoDBFormatHelpers.scala
@@ -1,8 +1,10 @@
 package services.data.dynamodb
 
-import java.util.UUID
+import java.nio.ByteBuffer
+import java.util.Base64
 
 import com.gu.scanamo._
+import models.domain.Gender.Gender
 import models.domain.{Gender, Person}
 
 /**
@@ -10,23 +12,20 @@ import models.domain.{Gender, Person}
   * Gender Enumerations and UUIDs
   */
 object DynamoDBFormatHelpers {
+
+  import boopickle.Default._
+
+  implicit val genderPickler = transformPickler((s: String) => Gender.withName(s))((g: Gender) => g.toString)
+
   implicit def personFormat: DynamoFormat[Person] = DynamoFormat.coercedXmap[Person, Map[String, String], Exception](
     map =>
-      Person(
-        UUID.fromString(map("id")),
-        map("firstName"),
-        map("lastName"),
-        map("studentId"),
-        Gender.withName(map("gender"))
-      )
+      Unpickle[Person].fromBytes(ByteBuffer.wrap(Base64.getDecoder.decode(map("data"))))
   )(
     person =>
       Map(
         "id" -> person.id.toString,
-        "firstName" -> person.firstName,
-        "lastName" -> person.lastName,
-        "studentId" -> person.studentId.toString,
-        "gender" -> person.gender.toString
+        "version" -> "1",
+        "data" -> new String(Base64.getEncoder.encode(Pickle.intoBytes(person).array()))
       )
   )
 }
